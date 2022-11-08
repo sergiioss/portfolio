9afb00ad338fe234674c39705d0fac7c8dbeddfd
sergiioss <sergiiptx@gmail.com>
2022-11-08 19:07:40 +0100

    Links a github y linkedin

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./Header.scss"
+import {NavLink} from 'react-router-dom'
 
 const Header = props =>{
 
     return(
         <div className="header">
             <p>
-                SOY EL HEADER
+                <a className="github" href='https://github.com/sergiioss' target="blank">Github</a>
+                <a className="github" href='https://www.linkedin.com/in/sergiosotosousa' target="blank">Linkedin</a>
             </p>
         </div>
     )
