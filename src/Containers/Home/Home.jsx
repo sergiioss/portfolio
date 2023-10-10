002187da73f64ba41f42baf9745c116496fa19d7
sergiioss <sergiiptx@gmail.com>
2023-10-10 19:40:53 +0200

    changes

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,5 +1,9 @@
 import './home.scss'
 import { Row,Col } from 'react-bootstrap'
+import {Image} from 'react-bootstrap'
+import img1 from '../../assets/img1.jpg'
+import img4 from '../../assets/img4.png'
+import {NavLink} from 'react-bootstrap'
 
 const Home = () => {
 
@@ -13,14 +17,47 @@ const Home = () => {
                 </span>
             </Col>
             <Row className="contain">
-                <Col className="paragraph" sm={4} md={4} xl={4}>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo voluptatum sed nesciunt, omnis quam consequatur corporis vero blanditiis fugit dolorum veritatis, cumque quasi labore modi, maiores ad repudiandae amet consequuntur.
+                <Col className="paragraph" sm={4} md={4} xl={4} xxl={4}>
+                    <Image src={img4} className="img"/>
+                    <Row className="links" sm={4} md={4} xl={4} xxl={4}>
+                        <NavLink className="link-image-demo" href="https://cambia-tu-fisico.vercel.app/" target="_blank">Hola que tal</NavLink>
+                        <NavLink className="link-image-github" href="https://cambia-tu-fisico.vercel.app/" target="_blank">Hola que tal</NavLink>
+                    </Row>
                 </Col>
-                <Col className="paragraph mid" sm={4} md={4} xl={4}>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo voluptatum sed nesciunt, omnis quam consequatur corporis vero blanditiis fugit dolorum veritatis, cumque quasi labore modi, maiores ad repudiandae amet consequuntur.
+                <Col className="paragraph"  sm={4} md={4} xl={4} xxl={4}>
+                    <Image src={img4} className="img"/>
+                    <Row className="links" sm={4} md={4} xl={4} xxl={4}>
+                        <NavLink className="link-image-demo" href="https://cambia-tu-fisico.vercel.app/" target="_blank">Hola que tal</NavLink>
+                        <NavLink className="link-image-github" href="https://cambia-tu-fisico.vercel.app/" target="_blank">Hola que tal</NavLink>
+                    </Row>
                 </Col>
-                <Col className="paragraph" sm={4} md={4} xl={4}>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo voluptatum sed nesciunt, omnis quam consequatur corporis vero blanditiis fugit dolorum veritatis, cumque quasi labore modi, maiores ad repudiandae amet consequuntur.
+                <Col className="paragraph"  sm={4} md={4} xl={4} xxl={4}>
+                    <Image src={img4} className="img"/>
+                    <Row className="links" sm={4} md={4} xl={4} xxl={4}>
+                        <NavLink className="link-image-demo" href="https://cambia-tu-fisico.vercel.app/" target="_blank">Hola que tal</NavLink>
+                        <NavLink className="link-image-github" href="https://cambia-tu-fisico.vercel.app/" target="_blank">Hola que tal</NavLink>
+                    </Row>
+                </Col>
+                <Col className="paragraph" sm={4} md={4} xl={4} xxl={4}>
+                    <Image src={img4} className="img"/>
+                    <Row className="links" sm={4} md={4} xl={4} xxl={4}>
+                        <NavLink className="link-image-demo" href="https://cambia-tu-fisico.vercel.app/" target="_blank">Hola que tal</NavLink>
+                        <NavLink className="link-image-github" href="https://cambia-tu-fisico.vercel.app/" target="_blank">Hola que tal</NavLink>
+                    </Row>
+                </Col>
+                <Col className="paragraph"  sm={4} md={4} xl={4} xxl={4}>
+                    <Image src={img4} className="img"/>
+                    <Row className="links" sm={4} md={4} xl={4} xxl={4}>
+                        <NavLink className="link-image-demo" href="https://cambia-tu-fisico.vercel.app/" target="_blank">Hola que tal</NavLink>
+                        <NavLink className="link-image-github" href="https://cambia-tu-fisico.vercel.app/" target="_blank">Hola que tal</NavLink>
+                    </Row>
+                </Col>
+                <Col className="paragraph"  sm={4} md={4} xl={4} xxl={4}>
+                    <Image src={img4} className="img"/>
+                    <Row className="links" sm={4} md={4} xl={4} xxl={4}>
+                        <NavLink className="link-image-demo" href="https://cambia-tu-fisico.vercel.app/" target="_blank">Hola que tal</NavLink>
+                        <NavLink className="link-image-github" href="https://cambia-tu-fisico.vercel.app/" target="_blank">Hola que tal</NavLink>
+                    </Row>
                 </Col>
             </Row>
         </Row>
