a7e4c17c5b9711d3bdc6cc299f4ce5990060c8bc
sergiioss <sergiiptx@gmail.com>
2023-12-07 18:27:25 +0100

    logo cv

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,6 +4,8 @@ import img3 from "../../Assets/img3.png"
 import img4 from "../../Assets/sergio.jpg"
 import {Row,Col } from 'react-bootstrap'
 import {NavLink} from 'react-bootstrap'
+import cv from '../../Assets/Cv.pdf'
+import imgCv from '../../Assets/imgCv.png'
 
 
 const Header = () => {
@@ -16,6 +18,7 @@ const Header = () => {
                 <Col className="links" xs={12} sm={6} md={6} xl={6}>
                     <NavLink className="github" href='https://github.com/sergiioss' target="blank"><img className="icon"href='https://github.com/sergiioss' src={img2} />Github</NavLink>
                     <NavLink className="github" href='https://www.linkedin.com/in/sergiosotosousa' target="blank"><img className="icon" src={img3} />Linkedin</NavLink>
+                    <NavLink className="github" href={cv} target="blank" download="Cv.pdf" src={imgCv}><img className="icon" />Cv</NavLink>
                 </Col>
         </Row>
     )
